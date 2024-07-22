569892a6ab362cac03faf2a753ce7ab6e1006c17
Rabih Kahaleh <rabih.kahaleh@balamand.edu.lb>
2024-07-22 16:16:30 +0300

    Bug #25 - Borrowing panel - become stuck - File Upload/Download

--- frontend/app/containers/FileDownload/index.js
+++ frontend/app/containers/FileDownload/index.js
@@ -1,62 +1,93 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react';
 import { createStructuredSelector } from 'reselect';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
-import {requestdownloadFile, cleanFileDownload} from './actions' 
-import fileDownloadSelector  from './selectors';
+import { requestdownloadFile, cleanFileDownload } from './actions';
+import fileDownloadSelector from './selectors';
 import FileDownloadForm from '../../components/FileDownloadForm';
-import downloadFile from 'large-file-downloader';
-import LoadingSpinner from "../LoadingSpinner";
+import LoadingSpinner from '../LoadingSpinner';
 
 const FileDownload = (props) => {
-    console.log("FileDownlad:", props)      
-    const {dispatch,filedownload, isLoading,filehash, libraryid, reqid}=props
-    const [downloaded,setdownloaded]=useState(null)
+  console.log("FileDownload props:", props);
+  const { dispatch, filedownload, isLoading, filehash, libraryid, reqid } = props;
+  const [downloaded, setDownloaded] = useState(false);
+  const [isDownloading, setIsDownloading] = useState(false);
 
-    const GetfiledownloadBase64 = () => {  
-      setdownloaded(false)  
-      if(filehash)
-           dispatch(requestdownloadFile(reqid, filehash,libraryid,"","",""))
+  const GetfiledownloadBase64 = () => {
+    setDownloaded(false);
+    setIsDownloading(true);
+    if (filehash) {
+      dispatch(requestdownloadFile(reqid, filehash, libraryid, "", "", ""));
+    }
+  };
+
+  const isValidBase64 = (str) => {
+    try {
+      return btoa(atob(str)) === str;
+    } catch (err) {
+      return false;
+    }
+  };
+
+  const downloadFileContent = (fileContent, fileName) => {
+    const binaryString = window.atob(fileContent);
+    const len = binaryString.length;
+    const bytes = new Uint8Array(len);
+    for (let i = 0; i < len; i++) {
+      bytes[i] = binaryString.charCodeAt(i);
     }
 
-    useEffect(() => {
-      if (!downloaded)
-      { 
-        downloadFile('data:application/octet-stream;base64,' + filedownload.filecontent, filehash)
-        setdownloaded(true)
+    const blob = new Blob([bytes], { type: 'application/octet-stream' });
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = fileName;  // Use filehash as filename
+    document.body.appendChild(a);
+    a.click();
+    a.remove();
+    window.URL.revokeObjectURL(url);
+    setDownloaded(true);
+    setIsDownloading(false);
+  };
+
+  useEffect(() => {
+    if (isDownloading && filedownload.filecontent && !downloaded) {
+      if (!filedownload.filecontent || !isValidBase64(filedownload.filecontent)) {
+        console.error('Invalid or empty file content.');
+        setIsDownloading(false);
+        return;
       }
-        
-    }, [filedownload.filecontent])
 
+      console.log('File content available. Downloading...');
+      downloadFileContent(filedownload.filecontent, filehash);  // Pass filehash as filename
+    }
+  }, [filedownload.filecontent, downloaded, isDownloading, filehash]);
 
-    useEffect(() => {
-      console.log("clear filedownload:")
-      //clean filedownloadbase64
+  useEffect(() => {
+    if (downloaded || (!isDownloading && filedownload.filecontent === null)) {
+      console.log("Clearing file download state");
       dispatch(cleanFileDownload());
-    }, [downloaded])
+    }
+  }, [downloaded, dispatch, isDownloading, filedownload.filecontent]);
 
+  return (
+    <>
+      <FileDownloadForm
+        FileDownloadCallBack={() => GetfiledownloadBase64()}
+        isLoading={isLoading}
+      />
+      {isDownloading && <LoadingSpinner />}
+    </>
+  );
+};
 
-
-return (
-  <>
-    <FileDownloadForm   
-          FileDownloadCallBack={ () => GetfiledownloadBase64()}                                        
-          isLoading={isLoading}
-    />              
-    {!downloaded && <LoadingSpinner />}
-  </>
-    )
-}
-
-const mapStateToProps = createStructuredSelector({  
+const mapStateToProps = createStructuredSelector({
   filedownload: fileDownloadSelector()
 });
 
-
-
 function mapDispatchToProps(dispatch) {
   return {
-      dispatch,
+    dispatch,
   };
 }
 
