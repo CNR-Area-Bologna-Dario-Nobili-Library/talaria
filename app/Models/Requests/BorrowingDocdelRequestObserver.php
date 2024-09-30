ce5024e9b40f4ec31ef849bfed26f9f0833b3714
Zaid1710 <zaid1234@hotmail.it>
2024-09-30 11:34:57 +0200

    Modified docdelrequest observers (both borrowing and lending) to index data to Elasticsearch

--- app/Models/Requests/BorrowingDocdelRequestObserver.php
+++ app/Models/Requests/BorrowingDocdelRequestObserver.php
@@ -4,6 +4,9 @@ use App\Models\BaseObserver;
 use \Auth;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Log;
+
+use App\Models\Requests\DocdelRequest;
 
 class BorrowingDocdelRequestObserver extends BaseObserver
 {
@@ -22,6 +25,30 @@ class BorrowingDocdelRequestObserver extends BaseObserver
 //        });
     }
 
+    protected function createLibraryObject($library)
+    {
+        return [
+            'id' => $library->id,
+            'name' => $library->name,
+            'country' => $library->country->only([
+                'id', 'name', 'code'
+            ]),
+            'subject' => $library->subject->only([
+                'id', 'name'
+            ]),
+            'institution' => [
+                'id' => $library->institution->id,
+                'name' => $library->institution->name,
+                'institution_type' => $library->institution->institution_type->only([
+                    'id', 'name'
+                ]),
+                'country' => $library->institution->country->only([
+                    'id', 'name', 'code'
+                ])
+            ]
+        ];
+    }
+
     public function creating($model)
     {
          //quando salvo viene messa in richiesta in quanto di default è status=requested         
@@ -38,18 +65,19 @@ class BorrowingDocdelRequestObserver extends BaseObserver
                 $model->operator_id=$userid;
             }
          }    
-            
+
          return parent::creating($model);
     }
 
     
     public function saving($model)
     {        
-        if($model->id) //il modello esiste già => sono in update!
+        if($model->id) { //il modello esiste già => sono in update!
             if(auth() && auth()->user()) {
                 $userid = auth()->user()->id;
                 $model->operator_id=$userid;
-            }        
+            }
+        }    
             
         if($model->isDirty('download'))
             $model->download_date=Carbon::now();
@@ -66,7 +94,7 @@ class BorrowingDocdelRequestObserver extends BaseObserver
         //when borrowing cancel request     
         if($model->isDirty('lending_archived'))
             $model->lending_archived_date=Carbon::now();        
-    
+
 
         return parent::saving($model);
 
@@ -74,8 +102,48 @@ class BorrowingDocdelRequestObserver extends BaseObserver
 
     public function saved($model)
     {
-        return parent::saved($model);
+        // After model is saved, let's index it to Elasticsearch
 
+        /** @var Elasticsearch\Client $client */
+        $client = app('Elasticsearch\Client');
+
+        // Elasticsearch request body params 
+        $params = [
+            'index' => 'docdelrequests',
+            'id' => $model->id,
+            'body' => [
+                'id' => $model->id,
+                'request_date' => \Carbon\Carbon::parse($model->request_date)->format('Y-m-d H:i:s'),
+                // 'fulfill_date' => \Carbon\Carbon::parse($model->fulfill_date)->format('Y-m-d H:i:s'),
+                'borrowing_status' => $model->borrowing_status,
+                'lending_status' => $model->lending_status,
+                // 'fulfill_type' => $model->fulfill_type,
+                // 'notfulfill_type' => $model->notfulfill_type,
+                'forward' => $model->forward,
+                'reference' => $model->reference->only([
+                    'id', 'material_type', 'pub_type', 'pub_title', 'pubyear', 'issn', 'isbn', 'oa_link'
+                ]),
+                'borrowing_library' => $this->createLibraryObject($model->borrowinglibrary),
+                'lending_library' => $model->lendinglibrary ? $this->createLibraryObject($model->lendinglibrary) : null
+            ]
+        ];
+
+        // Check if the request was newly created
+        if ($model->wasRecentlyCreated) {
+            // This is a new insertion, let's index it to elasticsearch
+            $client->index($params);
+        } else {
+            // This is an update, let's update it in elasticsearch
+            $client->update([
+                'index' => 'docdelrequests',
+                'id' => $model->id,
+                'body' => [
+                    'doc' => $params['body']
+                ]
+            ]);
+        }
+        
+        return parent::saved($model);
     }
 
     public function deleting($model)
@@ -87,5 +155,4 @@ class BorrowingDocdelRequestObserver extends BaseObserver
     {
         return parent::restoring($model);
     }
-
 }
--- app/Models/Requests/LendingDocdelRequestObserver.php
+++ app/Models/Requests/LendingDocdelRequestObserver.php
@@ -4,6 +4,7 @@ use App\Models\BaseObserver;
 use \Auth;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+// use Illuminate\Support\Facades\Log;
 
 class LendingDocdelRequestObserver extends BaseObserver
 {
@@ -13,12 +14,37 @@ class LendingDocdelRequestObserver extends BaseObserver
         'reference_id' => 'required|integer|exists:references,id',              
     ];
 
+    protected function createLibraryObject($library)
+    {
+        return [
+            'id' => $library->id,
+            'name' => $library->name,
+            'country' => $library->country->only([
+                'id', 'name', 'code'
+            ]),
+            'subject' => $library->subject->only([
+                'id', 'name'
+            ]),
+            'institution' => [
+                'id' => $library->institution->id,
+                'name' => $library->institution->name,
+                'institution_type' => $library->institution->institution_type->only([
+                    'id', 'name'
+                ]),
+                'country' => $library->institution->country->only([
+                    'id', 'name', 'code'
+                ])
+            ]
+        ];
+    }
+
     public function creating($model)
     {
         if(auth() && auth()->user()) {
             $userid = auth()->user()->id;
             $model->lending_operator_id=$userid;
         }
+
         return parent::creating($model);
     }
 
@@ -39,8 +65,38 @@ class LendingDocdelRequestObserver extends BaseObserver
 
     public function saved($model)
     {
-        return parent::saved($model);
+        /** @var Elasticsearch\Client $client */
+        $client = app('Elasticsearch\Client');
 
+        // Elasticsearch request body params + lending library in case of accepted orphaned request
+        $params = [
+            'index' => 'docdelrequests',
+            'id' => $model->id,
+            'body' => [
+                'id' => $model->id,
+                // 'request_date' => \Carbon\Carbon::parse($model->request_date)->format('Y-m-d H:i:s'),
+                'fulfill_date' => \Carbon\Carbon::parse($model->fulfill_date)->format('Y-m-d H:i:s'),
+                'borrowing_status' => $model->borrowing_status,
+                'lending_status' => $model->lending_status,
+                'fulfill_type' => $model->fulfill_type,
+                'notfulfill_type' => $model->notfulfill_type,
+                'forward' => $model->forward,
+                'reference' => $model->reference->only([
+                    'id', 'material_type', 'pub_type', 'pub_title', 'pubyear', 'issn', 'isbn', 'oa_link'
+                ]),
+                'lending_library' => $model->lendinglibrary ? $this->createLibraryObject($model->lendinglibrary) : null
+            ]
+        ];
+
+        $client->update([
+            'index' => 'docdelrequests',
+            'id' => $model->id,
+            'body' => [
+                'doc' => $params['body']
+            ]
+        ]);
+
+        return parent::saved($model);
     }
 
     public function deleting($model)
