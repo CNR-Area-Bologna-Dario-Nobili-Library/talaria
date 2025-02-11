bfdb2ad17f315a0c8de88e364ca7c1d7c8c04bae
TUGNOLI ALESSANDRO <alessandro.tugnoli@cnr.it>
2025-02-11 23:16:54 +0100

    fix text-wrap library tooltip

--- frontend/app/components/Library/LibraryTooltipContent/index.js
+++ frontend/app/components/Library/LibraryTooltipContent/index.js
@@ -9,15 +9,15 @@ const LibraryTooltipContent = (props) => {
     const intl = useIntl();
     
     return (
-            <div className='LibraryTooltipContent text-start text-nowrap'>                                       
-                        <div class="tooltip-header">
+            <div className='LibraryTooltipContent text-start'>                                       
+                        <div className="tooltip-header">
                             <h4 className='tooltip-title'>{data.name} {/* data.alt_name && data.alt_name!="" && <span className='alternative_name'><i className='fa-solid fa-tag'></i> {data.alt_name}</span> */}</h4>                        
                             
                             {data.institution && <h5 className='tooltip-subtitle text-muted'>{data.institution.data.name}</h5>}
                         </div>
                         <div className="tooltip-body">              
                             
-                            <div className='text-start text-nowrap'>
+                            <div className='text-start'>
                                 {data.country && <span className='country'>{data.country.data.name}&nbsp;</span>}
                                 {data.town && <span>{data.town}</span>}
                                 {data.ill_email && <div><i class="fa-solid fa-envelope"></i>&nbsp;{data.ill_email}</div>}
