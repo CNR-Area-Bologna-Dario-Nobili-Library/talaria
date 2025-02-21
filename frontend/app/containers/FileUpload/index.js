413db02d242dd872e89e9b6b367c44e51953cdcd
Rabih Kahaleh <rabihkahaleh@cnr.it>
2025-02-21 15:17:35 +0200

    FIX: "File uploaded successfully" text stuck

--- frontend/app/containers/FileUpload/index.js
+++ frontend/app/containers/FileUpload/index.js
@@ -1,83 +1,111 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from 'react';
 import { createStructuredSelector } from 'reselect';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
-import {useState} from 'react'
+import { useState } from 'react';
 
-import {cleanFileUpload, requestuploadFile} from './actions' 
-import fileUploadSelector  from './selectors';
-import {Card, Row, Col} from 'reactstrap';
+import { cleanFileUpload, requestuploadFile } from './actions';
+import fileUploadSelector from './selectors';
+import { Card, Row, Col } from 'reactstrap';
 import FileUploadForm from '../../components/FileUploadForm';
-import {useIntl} from 'react-intl';
-import LoadingSpinner from "../LoadingSpinner";
-const FileUpload = (props) => {
-  console.log("FileUpload:", props)      
-  const {dispatch, data, fileupload, cleanuploadprops}=props
-  const [Showspinner, setShowspinner] = useState(null)
-  const [ShowMessage, setShowMessage] = useState(null)
+import { useIntl } from 'react-intl';
+import LoadingSpinner from '../LoadingSpinner';
+
+
+const FileUpload = props => {
+  console.log('FileUpload:', props);
+  const { dispatch, data, fileupload, cleanuploadprops } = props;
+  const [Showspinner, setShowspinner] = useState(false);
+  const [ShowMessage, setShowMessage] = useState(false);
   const MaxFileUpload = process.env.MAX_UPLOAD_FILE;
-
   const AllowedFileTypes = process.env.FILE_EXTENSION;
-  const intl = useIntl()
+  const intl = useIntl();
+
   const uploadFile = (data, file, originalfilename, hideMessageFlag) => {
-      if (hideMessageFlag==1)
-      {
-        setShowspinner(true)
-        setShowMessage(false) //hide the message of "file upload succefully" or "File upload failed"
-        console.log(file)
-        console.log(originalfilename)
-        dispatch(requestuploadFile(props.data.id, props.data.lending_library_id, file, originalfilename, props.data.lending_status,"",""))
-      }
-      else
-        setShowMessage(true) //Show the message of "file upload succefully" or "File upload failed"
-  }
+    if (hideMessageFlag === 1) {
+      setShowspinner(true);
+      setShowMessage(false); // Hide message when new upload starts
+      console.log(file, originalfilename);
 
-  useEffect(() => {                
-    props.parentCallback(fileupload);
-    setShowspinner(false)
-  }, [fileupload])
+      // Reset previous fileupload status ONLY when starting a new upload
+      dispatch(cleanFileUpload());
 
+      dispatch(
+        requestuploadFile(
+          props.data.id,
+          props.data.lending_library_id,
+          file,
+          originalfilename,
+          props.data.lending_status,
+          '',
+          '',
+        ),
+      );
+    }
+  };
 
+  // Handle upload completion
   useEffect(() => {
-    if(cleanuploadprops)  // when cleanuploadprops is set to true, it dispatch the cleanfileupload. Otherwise do nothing
-      dispatch(cleanFileUpload())  
-  }, [cleanuploadprops])
-
-
-return (
-  <>
-    <FileUploadForm FileUploadCallBack={uploadFile} AllowedFileTypes={AllowedFileTypes}/>  
-    {(Showspinner) && <LoadingSpinner />}
-    {fileupload && !Showspinner && fileupload.status && <span className={"fileuploadstatusmessage"}>
-    { 
-          (fileupload.status=='uploaded') &&  (!ShowMessage) &&
-           <span className="text-success"><h4>{intl.formatMessage({id: "app.requests.UploadSuccess"})}</h4></span>    
+    if (fileupload && fileupload.status) {
+      props.parentCallback(fileupload);
+      setShowspinner(false);
+      setShowMessage(true); // Show message ONLY after upload completes
     }
-    {
-          (fileupload.status=='failed') &&  (!ShowMessage) &&
-          <span className="text-danger"><h4>{intl.formatMessage({id: "app.requests.UploadFail"})}</h4></span>                        
-    }
+  }, [fileupload]);
 
-    {
-          (fileupload.status=='The Uploaded File Exceeds the defined upload_max_filesize')  &&
-          <span className="text-danger"><h4>{intl.formatMessage({id: "app.requests.exceed_max_file_upload"})}</h4></span>                        
-    }
-    </span>
-    }
-    
-  </>
-    )
-}
+  // Reset states on component switch
+  useEffect(() => {
+    return () => {
+      setShowspinner(false);
+      setShowMessage(false);
+      dispatch(cleanFileUpload()); // Reset upload state when unmounting
+    };
+  }, [dispatch]);
 
-const mapStateToProps = createStructuredSelector({  
-  fileupload: fileUploadSelector()
+  return (
+    <>
+      <FileUploadForm
+        FileUploadCallBack={uploadFile}
+        AllowedFileTypes={process.env.FILE_EXTENSION}
+      />
+      {Showspinner && <LoadingSpinner />}
+      {fileupload && !Showspinner && fileupload.status && ShowMessage && (
+        <span className="fileuploadstatusmessage">
+          {fileupload.status === 'uploaded' && (
+            <span className="text-success">
+              <h4>
+                {intl.formatMessage({ id: 'app.requests.UploadSuccess' })}
+              </h4>
+            </span>
+          )}
+          {fileupload.status === 'failed' && (
+            <span className="text-danger">
+              <h4>{intl.formatMessage({ id: 'app.requests.UploadFail' })}</h4>
+            </span>
+          )}
+          {fileupload.status ===
+            'The Uploaded File Exceeds the defined upload_max_filesize' && (
+            <span className="text-danger">
+              <h4>
+                {intl.formatMessage({
+                  id: 'app.requests.exceed_max_file_upload',
+                })}
+              </h4>
+            </span>
+          )}
+        </span>
+      )}
+    </>
+  );
+};
+
+const mapStateToProps = createStructuredSelector({
+  fileupload: fileUploadSelector(),
 });
-
-
 
 function mapDispatchToProps(dispatch) {
   return {
-      dispatch,
+    dispatch,
   };
 }
 
@@ -86,4 +114,4 @@ const withConnect = connect(
   mapDispatchToProps,
 );
 
-export default compose(withConnect)((FileUpload));
+export default compose(withConnect)(FileUpload);
