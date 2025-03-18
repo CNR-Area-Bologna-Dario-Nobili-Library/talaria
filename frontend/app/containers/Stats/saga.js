ff43f30e2c4bd4b674d8273c23015c238b7658c5
Zaid <zaid.cheikh@area.bo.cnr.it>
2025-03-18 12:19:59 +0100

    added fetchFillRateSaga -- yet to finish refactoring

--- frontend/app/containers/Stats/saga.js
+++ frontend/app/containers/Stats/saga.js
@@ -1,61 +1,81 @@
-import { call, put, takeLatest } from 'redux-saga/effects';
-import { FETCH_AVG_TIME_REQUEST, FETCH_BORROWING_REQUESTS, FETCH_LIBRARIES_REQUEST, FETCH_COUNTRIES_REQUEST } from './constants';
-import { fetchAvgTimeSuccess, fetchAvgTimeFailure, fetchBorrowingRequestsSuccess, fetchBorrowingRequestsFailure, fetchLibrariesSuccess, fetchLibrariesFailure, fetchCountriesSuccess, fetchCountriesFailure } from './actions';
-import { admin_getAvgWorkingTime, admin_getBorrowingRequests } from '../../utils/apiAdmin';
+import { call, put, takeEvery, takeLatest } from 'redux-saga/effects';
+import { FETCH_AVG_TIME_REQUEST, FETCH_BORROWING_REQUESTS, FETCH_LIBRARIES_REQUEST, FETCH_COUNTRIES_REQUEST, FETCH_FILL_RATE_REQUEST } from './constants';
+import { fetchAvgTimeSuccess, fetchAvgTimeFailure, fetchBorrowingRequestsSuccess, fetchBorrowingRequestsFailure, fetchLibrariesSuccess, fetchLibrariesFailure, fetchCountriesSuccess, fetchCountriesFailure, fetchFillRateSuccess, fetchFillRateFailure } from './actions';
+import { admin_getAvgWorkingTime, admin_getBorrowingRequests, admin_getFillrate } from '../../utils/apiAdmin';
 import { getLibraryOptionList, getCountriesOptionsList } from '../../utils/api';
 
-function* fetchAvgTimeSaga(action) {
-  // console.log("fetchAvgTimeSaga", action);
+function* fetchFillRateSaga(action) {
+  console.log("fetchfillratesaga triggered with action:", action);
   try {
-    // console.log("fetchAvgTimeSaga A", action);
-    // console.log("fetchAvgTimeSaga", admin_getAvgWorkingTime);
-    // alert("Blabla");
-    // action.year = 2024;
-    const options = { year: action.year };
-    const data = yield call(admin_getAvgWorkingTime, options);
-    // console.log("fetchAvgTimeSaga B");
-    yield put(fetchAvgTimeSuccess(data));
+    const options = {
+      year: action.year,
+      library_id: action.library_id
+    };
+    const data = yield call(admin_getFillrate, options);
+    yield put(fetchFillRateSuccess(data));
   } catch (error) {
-    // alert("saga " + error.message);
-    console.log("fetchAvgTimeSaga failed:", error.message);
-    yield put(fetchAvgTimeFailure(error.message));
+    yield put(fetchFillRateFailure(error.message));
   }
 }
 
-export function* fetchBorrowingRequestsSaga(action) {
-  try {
-    const options = { year: action.year, borrowing_library_id: action.borrowing_library_id, material_type: action.material_type, borrowing_status: action.borrowing_status, fulfill_type: action.fulfill_type, notfulfill_type: action.notfulfill_type };
-    const data = yield call(admin_getBorrowingRequests, options);
-    yield put(fetchBorrowingRequestsSuccess(data));
-  } catch (error) {
-    yield put(fetchBorrowingRequestsFailure(error.message));
-  }
-}
+// OLD STUFF BELOW
+//! TO DELETE
 
-export function* fetchLibrariesSaga() {
-  try {
-    const options = {}
-    const data = yield call(getLibraryOptionList, options);
-    yield put(fetchLibrariesSuccess(data));
-  } catch (error) {
-    yield put(fetchLibrariesFailure(error.message));
-  }
-}
+// function* fetchAvgTimeSaga(action) {
+//   // console.log("fetchAvgTimeSaga", action);
+//   try {
+//     // console.log("fetchAvgTimeSaga A", action);
+//     // console.log("fetchAvgTimeSaga", admin_getAvgWorkingTime);
+//     // alert("Blabla");
+//     // action.year = 2024;
+//     const options = { year: action.year };
+//     const data = yield call(admin_getAvgWorkingTime, options);
+//     // console.log("fetchAvgTimeSaga B");
+//     yield put(fetchAvgTimeSuccess(data));
+//   } catch (error) {
+//     // alert("saga " + error.message);
+//     console.log("fetchAvgTimeSaga failed:", error.message);
+//     yield put(fetchAvgTimeFailure(error.message));
+//   }
+// }
 
-export function* fetchCountriesSaga() {
-  try {
-    const options = {}
-    const data = yield call(getCountriesOptionsList, options);
-    yield put(fetchCountriesSuccess(data));
-  } catch (error) {
-    yield put(fetchCountriesFailure(error.message));
-  }
-}
+// export function* fetchBorrowingRequestsSaga(action) {
+//   try {
+//     const options = { year: action.year, borrowing_library_id: action.borrowing_library_id, material_type: action.material_type, borrowing_status: action.borrowing_status, fulfill_type: action.fulfill_type, notfulfill_type: action.notfulfill_type };
+//     const data = yield call(admin_getBorrowingRequests, options);
+//     yield put(fetchBorrowingRequestsSuccess(data));
+//   } catch (error) {
+//     yield put(fetchBorrowingRequestsFailure(error.message));
+//   }
+// }
+
+// export function* fetchLibrariesSaga() {
+//   try {
+//     const options = {}
+//     const data = yield call(getLibraryOptionList, options);
+//     yield put(fetchLibrariesSuccess(data));
+//   } catch (error) {
+//     yield put(fetchLibrariesFailure(error.message));
+//   }
+// }
+
+// export function* fetchCountriesSaga() {
+//   try {
+//     const options = {}
+//     const data = yield call(getCountriesOptionsList, options);
+//     yield put(fetchCountriesSuccess(data));
+//   } catch (error) {
+//     yield put(fetchCountriesFailure(error.message));
+//   }
+// }
 
 export default function* statsSaga() {
   console.log("statsSaga");
-  yield takeLatest(FETCH_AVG_TIME_REQUEST, fetchAvgTimeSaga);
-  yield takeLatest(FETCH_BORROWING_REQUESTS, fetchBorrowingRequestsSaga);
-  yield takeLatest(FETCH_LIBRARIES_REQUEST, fetchLibrariesSaga);
-  yield takeLatest(FETCH_COUNTRIES_REQUEST, fetchCountriesSaga);
+  yield takeLatest(FETCH_FILL_RATE_REQUEST, fetchFillRateSaga);
+  // OLD STUFF BELOW
+  //! TO DELETE
+  // yield takeLatest(FETCH_AVG_TIME_REQUEST, fetchAvgTimeSaga);
+  // yield takeLatest(FETCH_BORROWING_REQUESTS, fetchBorrowingRequestsSaga);
+  // yield takeLatest(FETCH_LIBRARIES_REQUEST, fetchLibrariesSaga);
+  // yield takeLatest(FETCH_COUNTRIES_REQUEST, fetchCountriesSaga);
 } 
