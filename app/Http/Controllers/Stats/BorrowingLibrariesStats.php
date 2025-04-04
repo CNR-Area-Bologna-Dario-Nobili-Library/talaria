385b0d3432decb01529d063ed739118a91e699f1
Zaid <zaid.cheikh@area.bo.cnr.it>
2025-04-04 15:12:58 +0200

    index name change

--- app/Http/Controllers/Stats/BorrowingLibrariesStats.php
+++ app/Http/Controllers/Stats/BorrowingLibrariesStats.php
@@ -15,7 +15,7 @@ class BorrowingLibrariesStats extends BaseStatsController
   public function __invoke()
   {
     $params = [
-      'index' => 'docdel_test',
+      'index' => 'docdel_requests',
       'body'  => [
         'size' => 0, // no documents in returns, only aggregations
         'aggs' => [
