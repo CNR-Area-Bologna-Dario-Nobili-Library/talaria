8faf7f5899db05498f9b870f5ca58604a775822b
Zaid <zaid.cheikh@area.bo.cnr.it>
2025-04-04 15:11:13 +0200

    Initialize country_id filter + code cleanup

--- app/Http/Controllers/Stats/CountriesStats.php
+++ app/Http/Controllers/Stats/CountriesStats.php
@@ -21,35 +21,36 @@ class CountriesStats extends BaseStatsController
     // Validate optional parameters 'year', 'library_id' and 'country_id'
     $validated = $request->validate([
       'year' => 'sometimes|integer|min:2020|max:' . date('Y'),
-      'library_id' => 'sometimes|integer|exists:libraries,id', // Using talaria normally
-      'country_id' => 'sometimes|integer|exists:countries,id',  // Using talaria normally
+      'library_id' => 'sometimes|integer|exists:libraries,id',
       'institution_id' => 'sometimes|integer|exists:institutions,id',
-      // 'library_id' => 'sometimes|integer',  // Using RSCVD prod for stats
-      // 'country_id' => 'sometimes|integer'   // Using RSCVD prod for stats
+      'country_id' => 'sometimes|integer|exists:countries,id',
+      // 'library_id' => 'sometimes|integer',
+      // 'institution_id' => 'sometimes|integer',
+      // 'country_id' => 'sometimes|integer',
     ]);
 
     $year = $validated['year'] ?? null;
     $library_id = $validated['library_id'] ?? null;
-    $country_id = $validated['country_id'] ?? null;
     $institution_id = $validated['institution_id'] ?? null;
+    $country_id = $validated['country_id'] ?? null;
 
     $borrowing_query_condition = null;
     $lending_query_condition = null;
     $query_id = null;
 
-    // Priority: Library >> Country >> Institution
+    // Priority: Library >> Institution >> Country
     if ($library_id) {
       $borrowing_query_condition = "borrowing_library.id";
       $lending_query_condition = "lending_library.id";
       $query_id = $library_id;
-    } else if ($country_id) {
-      $borrowing_query_condition = "borrowing_library.country.id";
-      $lending_query_condition = "lending_library.country.id";
-      $query_id = $country_id;
     } else if ($institution_id) {
       $borrowing_query_condition = "borrowing_library.institution.id";
       $lending_query_condition = "lending_library.institution.id";
       $query_id = $institution_id;
+    } else if ($country_id) {
+      $borrowing_query_condition = "borrowing_library.country.id";
+      $lending_query_condition = "lending_library.country.id";
+      $query_id = $country_id;
     }
 
     $query = [
@@ -61,7 +62,7 @@ class CountriesStats extends BaseStatsController
     ];
 
     // I AM BORROWER AND I SEARCH WHO FULFILLS MY REQUESTS 
-    $query['body']['aggs']['providing'] = [
+    $query['body']['aggs']['requesting'] = [
       'filter' => [
         'bool' => [
           'must' => array_filter(
@@ -73,7 +74,7 @@ class CountriesStats extends BaseStatsController
         ]
       ],
       'aggs' => [
-        'countries_requested_from' => [
+        'countries_providing_to_me' => [
           'terms' => [
             'field' => 'lending_library.country.name.keyword',
             'size' => 100
@@ -97,7 +98,7 @@ class CountriesStats extends BaseStatsController
     ];
 
     // I AM LENDER AND I SEARCH WHO ASKS ME DOCUMENTS
-    $query['body']['aggs']['requesting'] = [
+    $query['body']['aggs']['providing'] = [
       'filter' => [
         'bool' => [
           'must' => array_filter(
@@ -109,7 +110,7 @@ class CountriesStats extends BaseStatsController
         ]
       ],
       'aggs' => [
-        'countries_requested_from' => [
+        'countries_requesting_from_me' => [
           'terms' => [
             'field' => 'borrowing_library.country.name.keyword',
             'size' => 100
@@ -148,20 +149,20 @@ class CountriesStats extends BaseStatsController
     $response = $this->client->search($query);
     $result = [
       "total_requests" => $response["hits"]["total"]["value"],
-      "requesting" => [
+      "requesting_from" => [
         "total" => $response["aggregations"]["requesting"]["doc_count"],
         "countries" => []
       ],
-      "providing" => [
+      "providing_to" => [
         "total" => $response["aggregations"]["providing"]["doc_count"],
         "countries" => []
       ]
     ];
 
-    // Extract requesting countries
-    foreach ($response["aggregations"]["providing"]["countries_requested_from"]["buckets"] as $bucket) {
+    // Extract "requesting from" countries
+    foreach ($response["aggregations"]["requesting"]["countries_providing_to_me"]["buckets"] as $bucket) {
       $country_data = $bucket["country_details"]["hits"]["hits"][0]["_source"]["lending_library"]["country"];
-      $result["providing"]["countries"][] = [
+      $result["requesting_from"]["countries"][] = [
         // "id" => $country_data["id"],
         "name" => $country_data["name"],
         "code" => $country_data["code"],
@@ -169,10 +170,10 @@ class CountriesStats extends BaseStatsController
       ];
     }
 
-    // Extract providing countries
-    foreach ($response["aggregations"]["requesting"]["countries_requested_from"]["buckets"] as $bucket) {
+    // Extract "providing to" countries
+    foreach ($response["aggregations"]["providing"]["countries_requesting_from_me"]["buckets"] as $bucket) {
       $country_data = $bucket["country_details"]["hits"]["hits"][0]["_source"]["borrowing_library"]["country"];
-      $result["requesting"]["countries"][] = [
+      $result["providing_to"]["countries"][] = [
         // "id" => $country_data["id"],
         "name" => $country_data["name"],
         "code" => $country_data["code"],
