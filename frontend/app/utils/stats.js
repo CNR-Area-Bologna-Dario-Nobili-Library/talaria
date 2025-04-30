389f809486f962a47d8063e9b301cedebba5ffa5
Zaid <zaid.cheikh@area.bo.cnr.it>
2025-04-30 15:53:45 +0200

    updated stats frontend utils to have mappings for deliverymethod and reasonunfilled

--- frontend/app/utils/stats.js
+++ frontend/app/utils/stats.js
@@ -1,6 +1,4 @@
-export default function getMaterialTypeLabel(materialTypeId, intl) {
-  // console.log('getMaterialTypeLabel', materialTypeId);
-
+export function getMaterialTypeLabel(materialTypeId, intl) {
   switch (materialTypeId) {
     case '1':
       return intl.formatMessage({ id: 'app.references.article' });
@@ -12,6 +10,46 @@ export default function getMaterialTypeLabel(materialTypeId, intl) {
       return intl.formatMessage({ id: 'app.references.cartography' });
     case '5':
       return intl.formatMessage({ id: 'app.references.manuscript' });
+    default:
+      return null;
+  }
+}
+
+export function getDeliveryMethodLabel(deliveryMethodId, intl) {
+  switch (deliveryMethodId) {
+    case 1:
+      return intl.formatMessage({ id: 'app.requests.deliveryMethod.file' });
+    case 2:
+      return intl.formatMessage({ id: 'app.requests.deliveryMethod.mail' });
+    case 3:
+      return intl.formatMessage({ id: 'app.requests.deliveryMethod.fax' });
+    case 4:
+      return intl.formatMessage({ id: 'app.requests.deliveryMethod.url' });
+    case 5:
+      return intl.formatMessage({ id: 'app.requests.deliveryMethod.articleexchange' });
+    case 6:
+      return intl.formatMessage({ id: 'app.requests.deliveryMethod.other' });
+    default:
+      return null;
+  }
+}
+
+export function getReasonUnfilledLabel(reasonUnfilledId, intl) {
+  switch (reasonUnfilledId) {
+    case 1:
+      return intl.formatMessage({ id: 'app.requests.notfulfill_type.NotAvailableForILL' });
+    case 2:
+      return intl.formatMessage({ id: 'app.requests.notfulfill_type.NotHeld' });
+    case 3:
+      return intl.formatMessage({ id: 'app.requests.notfulfill_type.NotOnShelf' });
+    case 4:
+      return intl.formatMessage({ id: 'app.requests.notfulfill_type.ILLNotPermittedByLicense' });
+    case 5:
+      return intl.formatMessage({ id: 'app.requests.notfulfill_type.WrongRef' });
+    case 6:
+      return intl.formatMessage({ id: 'app.requests.notfulfill_type.MaxReqNumber' });
+    case 7:
+      return intl.formatMessage({ id: 'app.requests.notfulfill_type.Other' });
     default:
       return null;
   }
