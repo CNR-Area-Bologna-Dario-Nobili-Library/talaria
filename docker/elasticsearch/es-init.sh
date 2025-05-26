bf83ba1c7842888671a918379c6ed7956d40ac94
Zaid <zaid.cheikh@area.bo.cnr.it>
2025-05-26 14:29:34 +0200

    Added elasticsearch shell script timeout after 60seconds + dynamic url

--- docker/elasticsearch/es-init.sh
+++ docker/elasticsearch/es-init.sh
@@ -11,20 +11,28 @@ if [ -f "$INIT_MARKER" ]; then
 fi
 
 # Wait for elasticsearch to start
+MAX_RETRIES=20 # 20 x 3 = 60 seconds (1 minute to wait for Elasticsearch to start)
+RETRY_COUNT=0
+
 echo "Waiting for elasticsearch to start..."
-until curl -k -s -u elastic:$ELASTIC_PASSWORD "https://elasticsearch:9200" > /dev/null; do
+until curl -k -s -u elastic:$ELASTIC_PASSWORD "$ELASTIC_HOST" > /dev/null; do
   sleep 3
+  RETRY_COUNT=$((RETRY_COUNT + 1))
+  if [ $RETRY_COUNT -ge $MAX_RETRIES ]; then
+    echo "Timed out waiting for Elasticsearch to start. Exiting."
+    exit 1
+  fi
 done
 
 echo "Elasticsearch is up and running."
 
 # Disable disk allocation threshold
-curl -k -u "elastic:$ELASTIC_PASSWORD" -X PUT "https://elasticsearch:9200/_cluster/settings" -H "Content-Type: application/json" -d '{"persistent":{"cluster.routing.allocation.disk.threshold_enabled":false}}'
+curl -k -u "elastic:$ELASTIC_PASSWORD" -X PUT "$ELASTIC_HOST/_cluster/settings" -H "Content-Type: application/json" -d '{"persistent":{"cluster.routing.allocation.disk.threshold_enabled":false}}'
 
 echo "Disk allocation threshold disabled."
 
 # Set kibana_system user password
-curl -k -u "elastic:$ELASTIC_PASSWORD" -X POST "https://elasticsearch:9200/_security/user/kibana_system/_password" -H "Content-Type: application/json" -d "{\"password\":\"$KIBANA_PASSWORD\"}"
+curl -k -u "elastic:$ELASTIC_PASSWORD" -X POST "$ELASTIC_HOST/_security/user/kibana_system/_password" -H "Content-Type: application/json" -d "{\"password\":\"$KIBANA_PASSWORD\"}"
 
 echo "kibana_system user password set."
 
