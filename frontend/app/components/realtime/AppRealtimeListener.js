bb04aa23217bcf82adb198d00a537d86d045ea66
Rabih Kahaleh <rabihkahaleh@cnr.it>
2025-11-14 09:42:19 +0200

    FIX:Issue310_Notifications: Refresh Wrong Panel

--- frontend/app/components/realtime/AppRealtimeListener.js
+++ frontend/app/components/realtime/AppRealtimeListener.js
@@ -146,39 +146,83 @@ const AppRealtimeListener = function AppRealtimeListener(props) {
         log(TAG, 'Starting scenario checks...');
 
         // SCENARIO 1 Only refresh wgen Actor update on lending -> refresh borrowing on the target side 
-        var s1_hasBorrowing = pathname.indexOf('/borrowing') !== -1;
-        var s1_notRequest = !pathname.match(/\/borrowing\/\d+\/request/);
-        var s1_noLending = pathname.indexOf('/lending') === -1;
-        var s1_noToDeliver = pathname.indexOf('/to-deliver') === -1;
-        var s1_hasLibId = !!targetCurrentLibId;
+        // var s1_hasBorrowing = pathname.indexOf('/borrowing') !== -1;
+        // var s1_notRequest = !pathname.match(/\/borrowing\/\d+\/request/);
+        // var s1_noLending = pathname.indexOf('/lending') === -1;
+        // var s1_noToDeliver = pathname.indexOf('/to-deliver') === -1;
+        // var s1_hasLibId = !!targetCurrentLibId;
+        // var s1_correctLibrary = borrowingLibId && targetCurrentLibId === borrowingLibId;
+        // log(TAG,'SCENARIO 1 CHECK: /borrowing?',s1_hasBorrowing, '!request?', s1_notRequest, '!lending?', s1_noLending, '!to-deliver?', s1_noToDeliver, 'libId?', s1_hasLibId,   'correctLib (borrowing)?',  s1_correctLibrary, );
+        // if (s1_hasBorrowing && s1_notRequest && s1_noLending && s1_noToDeliver && s1_hasLibId && s1_correctLibrary) {
+        //   log(TAG, 'SCENARIO 1 MATCHED: Refreshing borrowing list for lib', targetCurrentLibId,);
+        //   clearTimeout(tBorrow);
+        //   tBorrow = setTimeout(function() {
+        //     refreshBorrower(targetCurrentLibId, false);
+        //   }, 300);
+        //   return; 
+        // }
+        // log(TAG, 'SCENARIO 1 SKIPPED');
+
+        // SCENARIO 1: Only refresh when on the BORROWING LIST page
+        // e.g. /library/25/borrowing or /library/25/borrowing/
+        var s1_isBorrowingList = /\/library\/\d+\/borrowing\/?$/.test(pathname);
+        var s1_noLending      = pathname.indexOf('/lending') === -1;
+        var s1_noToDeliver    = pathname.indexOf('/to-deliver') === -1;
+        var s1_hasLibId       = !!targetCurrentLibId;
         var s1_correctLibrary = borrowingLibId && targetCurrentLibId === borrowingLibId;
-        log(TAG,'SCENARIO 1 CHECK: /borrowing?',s1_hasBorrowing, '!request?', s1_notRequest, '!lending?', s1_noLending, '!to-deliver?', s1_noToDeliver, 'libId?', s1_hasLibId,   'correctLib (borrowing)?',  s1_correctLibrary, );
-        if (s1_hasBorrowing && s1_notRequest && s1_noLending && s1_noToDeliver && s1_hasLibId && s1_correctLibrary) {
-          log(TAG, 'SCENARIO 1 MATCHED: Refreshing borrowing list for lib', targetCurrentLibId,);
+
+        log(TAG, 'SCENARIO 1 CHECK:', 'isBorrowingList?', s1_isBorrowingList, '!lending?', s1_noLending, '!to-deliver?', s1_noToDeliver,  'libId?',  s1_hasLibId,'correctLib (borrowing)?', s1_correctLibrary,  );
+
+        if (s1_isBorrowingList && s1_noLending && s1_noToDeliver && s1_hasLibId && s1_correctLibrary) {
+          log(TAG, 'SCENARIO 1 MATCHED: Refreshing borrowing LIST for lib', targetCurrentLibId,);
           clearTimeout(tBorrow);
-          tBorrow = setTimeout(function() {
-            refreshBorrower(targetCurrentLibId, false);
-          }, 300);
-          return; 
+          tBorrow = setTimeout(function () {
+              refreshBorrower(targetCurrentLibId, false);
+            }, 300);
+          return;
         }
+
         log(TAG, 'SCENARIO 1 SKIPPED');
 
 
         // SCENARIO 2: Only refresh if the notification's lending_library_id matches target's library
-        var s2_hasLending = pathname.indexOf('/lending') !== -1 || pathname.indexOf('/to-deliver') !== -1;
-        var s2_noBorrowing = pathname.indexOf('/borrowing') === -1;
-        var s2_hasLibId = !!targetCurrentLibId;
+        // var s2_hasLending = pathname.indexOf('/lending') !== -1 || pathname.indexOf('/to-deliver') !== -1;
+        // var s2_noBorrowing = pathname.indexOf('/borrowing') === -1;
+        // var s2_hasLibId = !!targetCurrentLibId;
+        // var s2_correctLibrary = lendingLibId && targetCurrentLibId === lendingLibId;
+        // log(TAG, 'SCENARIO 2 CHECK: /lending or /to-deliver?',s2_hasLending, '!borrowing?', s2_noBorrowing, 'libId?', s2_hasLibId, 'correctLib (lending)?', s2_correctLibrary, );
+        // if (s2_hasLending && s2_noBorrowing && s2_hasLibId && s2_correctLibrary) {
+        //   log(TAG, 'SCENARIO 2 MATCHED: Refreshing lending list for lib',targetCurrentLibId,);
+        //   clearTimeout(tLender);
+        //   tLender = setTimeout(function() {
+        //     refreshLender(targetCurrentLibId);
+        //   }, 300);
+        //   return;
+        // }
+        // log(TAG, 'SCENARIO 2 SKIPPED');
+        // SCENARIO 2: Only refresh when on the LENDING / TO-DELIVER LIST page
+        // e.g. /library/25/lending or /library/25/lending/
+        //      /library/25/to-deliver or /library/25/to-deliver/
+        var s2_isLendingList =
+        /\/library\/\d+\/lending\/?$/.test(pathname) ||
+        /\/library\/\d+\/to-deliver\/?$/.test(pathname);
+
+        var s2_noBorrowing    = pathname.indexOf('/borrowing') === -1;
+        var s2_hasLibId       = !!targetCurrentLibId;
         var s2_correctLibrary = lendingLibId && targetCurrentLibId === lendingLibId;
-        log(TAG, 'SCENARIO 2 CHECK: /lending or /to-deliver?',s2_hasLending, '!borrowing?', s2_noBorrowing, 'libId?', s2_hasLibId, 'correctLib (lending)?', s2_correctLibrary, );
-        if (s2_hasLending && s2_noBorrowing && s2_hasLibId && s2_correctLibrary) {
-          log(TAG, 'SCENARIO 2 MATCHED: Refreshing lending list for lib',targetCurrentLibId,);
-          clearTimeout(tLender);
-          tLender = setTimeout(function() {
+
+        log(TAG,'SCENARIO 2 CHECK:', 'isLendingList?', s2_isLendingList, '!borrowing?',    s2_noBorrowing, 'libId?',         s2_hasLibId, 'correctLib (lending)?', s2_correctLibrary,);
+
+        if (s2_isLendingList && s2_noBorrowing && s2_hasLibId && s2_correctLibrary) {
+        log(TAG, 'SCENARIO 2 MATCHED: Refreshing LENDING LIST for lib', targetCurrentLibId,);
+        clearTimeout(tLender);
+        tLender = setTimeout(function () {
             refreshLender(targetCurrentLibId);
           }, 300);
           return;
         }
-        log(TAG, 'SCENARIO 2 SKIPPED');
+       log(TAG, 'SCENARIO 2 SKIPPED');
+
 
         // Scenario 3: https://talaria.local/library/XXX/borrowing/YYY/request -> refresh target user lending list
         log(TAG, 'SCENARIO 3 CHECK: Testing path:', pathname);
