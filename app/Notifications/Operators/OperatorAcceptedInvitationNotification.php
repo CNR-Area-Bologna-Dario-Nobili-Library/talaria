d09e48e1935b6fbaad0b09d1c9b0e4e2e4d3cbc8
TUGNOLI ALESSANDRO <alessandro.tugnoli@cnr.it>
2025-11-27 10:56:09 +0100

    operator invitation notification: fix notification url for accepting/rejecting

--- app/Notifications/Operators/OperatorAcceptedInvitationNotification.php
+++ app/Notifications/Operators/OperatorAcceptedInvitationNotification.php
@@ -19,7 +19,18 @@ class OperatorAcceptedInvitationNotification extends MandatoryNotification
     {                
         parent::__construct();
         $this->object=$model;
-        $this->url=config('app.frontend_url').'/user/dashboard';       
+        
+        $entity_dashboard_url="";
+
+        switch($this->object->entity_type) {
+            case 'App\\Models\\Libraries\\Library': $entity_dashboard_url='/library/'.$this->object->entity_id; break;
+            case 'App\\Models\\Institutions\\Institution':  $entity_dashboard_url='/institution/'.$this->object->entity_id; break;
+            case 'App\\Models\\Projects\\Project': $entity_dashboard_url='/project/'.$this->object->entity_id; break;
+            case 'App\\Models\\Institutions\\Consortium': $entity_dashboard_url='/consortium/'.$this->object->entity_id; break;
+        }
+    
+
+        $this->url=config('app.frontend_url').$entity_dashboard_url.'/manage/operators';       
         
         if($model->user_id!=null) { //user exists
             $user=$this->object->user;        
