1ac2f0eb299000e13e0b760c14cd87213f279802
TUGNOLI ALESSANDRO <alessandro.tugnoli@cnr.it>
2025-12-09 23:54:18 +0100

    FIX APP_URL to be used with assets in blade templates

--- config/filesystems.php
+++ config/filesystems.php
@@ -51,7 +51,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL').'/storage', /* NOTE: we don't use this variables so no matter where it points to */
             'visibility' => 'public',
         ],
 
