aae7c7e761419f53df84454cd7f4ef63adf6aebc
TUGNOLI ALESSANDRO <alessandro.tugnoli@cnr.it>
2025-12-10 23:55:28 +0100

    FIX APP_URL 2nd try

--- config/app.php
+++ config/app.php
@@ -57,7 +57,7 @@ return [
     'url' => env('APP_URL', 'http://localhost'),
 
      /* This is used by asset() in blade template */
-    'asset_url' => env('APP_URL', null), //env('ASSET_URL', null),
+    'asset_url' => env('API_URL', null), //env('ASSET_URL', null),
 
     /*
     |--------------------------------------------------------------------------
