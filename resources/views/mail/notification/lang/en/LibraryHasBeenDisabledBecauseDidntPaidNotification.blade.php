2285aaaff0d090e7a132f5577132fb733f771691
Zaid <zaid.cheikh@area.bo.cnr.it>
2026-02-16 12:37:47 +0100

    english fix in email blade

--- resources/views/mail/notification/lang/en/LibraryHasBeenDisabledBecauseDidntPaidNotification.blade.php
+++ resources/views/mail/notification/lang/en/LibraryHasBeenDisabledBecauseDidntPaidNotification.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')    
     <p>
-    The library {{$name}} has been disabled because didn't paid. 
+    The library {{$name}} has been disabled because of missing payment. 
     </p>
     
     <p class="contacts">@lang('notification.comm_manager_contacts')</p>
