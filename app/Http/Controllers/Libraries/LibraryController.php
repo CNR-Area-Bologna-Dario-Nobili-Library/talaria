3adc456b43daaa386b01d0ac455919063db5a217
Rabih Kahaleh <rabihkahaleh@cnr.it>
2026-02-28 12:14:37 +0200

    FIX_Issue_#390: Error when modifying library data AS COMMUNITY MANAGER/SUPER-ADMIN

--- app/Http/Controllers/Libraries/LibraryController.php
+++ app/Http/Controllers/Libraries/LibraryController.php
@@ -456,10 +456,10 @@ class LibraryController extends ApiController
         //Convert coordinated to decimal if needed
         $lon = $request->input('lon');
         $lat = $request->input('lat');
-        if ($request->has('lon') && !is_numeric($lon))
+        if ($request->has('lon') && !is_null($lon) && !is_numeric($lon))
             $lon = Helper::convertCoordinateToDecimal($lon);
 
-        if ($request->has('lat') && !is_numeric($lat)) 
+        if ($request->has('lat') && !is_null($lat) && !is_numeric($lat))
             $lat = Helper::convertCoordinateToDecimal($lat);
         $model->lon = $lon ?? $model->lon;
         $model->lat = $lat ?? $model->lat;
